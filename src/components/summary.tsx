184921ed643c55b44f7544043c8c43b0493d6dea
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-25 17:44:07 -0300

    feat: add price formatter on values

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -1,6 +1,10 @@
+import { useSummary } from '@/hooks/useSummary'
+import { priceFormatter } from '@/utils/formatter'
 import { ArrowDownCircle, ArrowUpCircle, DollarSign } from 'lucide-react'
 
 export default function Summary() {
+  const { income, outcome, total } = useSummary()
+
   return (
     <section className="mx-auto -mt-20 grid w-full max-w-6xl grid-cols-3 gap-2 px-6">
       <div className="rounded-md bg-zinc-800 p-8 shadow-shape">
@@ -9,7 +13,9 @@ export default function Summary() {
           <ArrowUpCircle className="size-8 text-emerald-500" />
         </header>
 
-        <strong className="mt-4 block text-4xl">R$ 17.400,00</strong>
+        <strong className="mt-4 block text-4xl">
+          {priceFormatter.format(income)}
+        </strong>
       </div>
 
       <div className="rounded-md bg-zinc-800 p-8 shadow-shape">
@@ -18,7 +24,9 @@ export default function Summary() {
           <ArrowDownCircle className="size-8 text-red-500" />
         </header>
 
-        <strong className="mt-4 block text-4xl">R$ 5.120,80</strong>
+        <strong className="mt-4 block text-4xl">
+          {priceFormatter.format(outcome)}
+        </strong>
       </div>
 
       <div className="rounded-md bg-emerald-600 p-8 shadow-shape">
@@ -27,7 +35,9 @@ export default function Summary() {
           <DollarSign className="size-8 text-white" />
         </header>
 
-        <strong className="mt-4 block text-4xl">R$ 48.090,27</strong>
+        <strong className="mt-4 block text-4xl">
+          {priceFormatter.format(total)}
+        </strong>
       </div>
     </section>
   )
