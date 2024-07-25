c8ccdee7f0e787a3fef59ca020a59dd2154e75ee
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-25 17:41:59 -0300

    refactor: change the table style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { format } from 'date-fns'
 import Header from '@/components/header'
 import SearchForm from '@/components/search-form'
 import Summary from '@/components/summary'
+import { priceFormatter } from '@/utils/formatter'
 
 export default function Home() {
   const { transactions } = useTransaction()
@@ -15,23 +16,21 @@ export default function Home() {
       <Header />
       <Summary />
 
-      <main className="mx-auto mt-16 w-full max-w-6xl space-y-6 px-6">
+      <main className="mx-auto mt-16 max-w-6xl space-y-6 px-6">
         <SearchForm />
 
-        <table className="w-full border-collapse border-spacing-2">
+        <table className="w-full flex-1 border-collapse border-spacing-2">
           <tbody className="w-full space-y-2">
             {transactions.map((transaction) => {
               return (
                 <tr
                   key={transaction.id}
-                  className="flex w-full rounded-md bg-zinc-800 hover:bg-zinc-800/80"
+                  className="w-full max-w-full rounded-md bg-zinc-900 hover:bg-zinc-800/80"
                 >
-                  <td className="flex-1 px-8 py-5">
-                    {transaction.description}
-                  </td>
+                  <td className="w-1/2 px-8 py-5">{transaction.description}</td>
                   <td className="px-8 py-5 text-left">
                     <PriceHighlight variant={transaction.type}>
-                      R$ {transaction.price}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td className="px-8 py-5 text-left">
