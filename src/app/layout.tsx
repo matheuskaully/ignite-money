559d56e53ca66db1e461be88a9f3d5fb059022be
Matheus Kaúlly <kaully.santos@gmail.com>
2024-07-25 17:41:18 -0300

    feat: add context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { TransactionsProvider } from '@/contexts/transactions-context'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} bg-zinc-950 text-zinc-50`}>
-        {children}
+        <TransactionsProvider>{children}</TransactionsProvider>
       </body>
     </html>
   )
