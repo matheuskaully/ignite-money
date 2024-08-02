39cb0f7e80326ec892ade34f0f73f96c2e3f8cfa
Matheus Kaúlly <kaully.santos@gmail.com>
2024-08-02 17:30:31 -0300

    feat: add sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { TransactionsProvider } from '@/contexts/transactions-context'
+import { Toaster } from 'sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} bg-zinc-950 text-zinc-50`}>
+        <Toaster richColors />
         <TransactionsProvider>{children}</TransactionsProvider>
       </body>
     </html>
